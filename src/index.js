9ada974c4cda12d70d368b59743f7531cd70964d
nas233 <nas233@gmail.com>
2024-08-09 01:35:42 +0400

    feat: add morse function

--- src/index.js
+++ src/index.js
@@ -38,6 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    let decExp = '';
+    for (let i = 0; i < expr.length; i+= 10){
+        let x = expr.slice(i, i + 10);
+        if ( x === '**********'){
+            decExp += ' ';
+        }else{
+            let morse = '';
+
+            for (let j = 0; j < 10; j+= 2){
+                let p = x.slice(j, j + 2);
+                if ( p === '10'){
+                    morse += '.';
+                }else if(p === '11'){
+                    morse += '-';
+                }                     
+            }
+
+            decExp += MORSE_TABLE[morse];
+        }       
+
+    }
+    return decExp
     // write your solution here
 }
 
